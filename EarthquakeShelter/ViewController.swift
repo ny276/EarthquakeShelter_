e2a3c459fcc9b7e46a7820d36432b3ea6af2684f
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-11-06 19:51:13 +0900

    //

--- EarthquakeShelter/ViewController.swift
+++ EarthquakeShelter/ViewController.swift
@@ -76,13 +76,9 @@ class ViewController: UIViewController, MKMapViewDelegate, XMLParserDelegate {
     
     
     func zoomToRegion() {
-        
-        print("zoom to Location")
-        
-        // 부산지도 구글 중심정 35.163246, 129.066297
-        
-        let location = CLLocationCoordinate2D(latitude: 35.163246, longitude: 129.066297)
-        let region = MKCoordinateRegionMakeWithDistance(location, 26000.0, 26000.3)
+        let location = CLLocationCoordinate2D(latitude: 35.180100, longitude: 129.081017)
+        let span = MKCoordinateSpan(latitudeDelta: 0.0027, longitudeDelta: 0.0027)
+        let region = MKCoordinateRegion(center: location, span: span)
         myMapView.setRegion(region, animated: true)
     }
     
