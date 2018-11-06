ff2b38fe1e61a0806414dde86902be3aea704e6b
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-11-06 17:48:03 +0900

    ver.2

--- BusanData.swift
+++ BusanData.swift
@@ -13,14 +13,12 @@ class BusanData: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var subtitle: String?
-    var pm10: String?
-    var pm10Cai: String?
     
-    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String, pm10: String, pm10Cai: String) {
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
         self.coordinate = coordinate
         self.title = title
         self.subtitle = subtitle
-        self.pm10 = pm10
-        self.pm10Cai = pm10Cai
+   
     }
 }
